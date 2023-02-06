db4026a5ea6fc5d8c02e31e2d79a09962da7de0c
Pedro Soares <ngrpedro22@gmail.com>
2023-02-06 14:38:34 -0300

    no history

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -11,47 +11,7 @@ const History = () => {
     <HistoryContainer>
       <h1>Meu histórico</h1>
 
-      <HistoryList>
-        <table>
-          <thead>
-            <tr>
-              <th>Tarefa</th>
-              <th>Duração</th>
-              <th>Duração</th>
-              <th>Status</th>
-            </tr>
-          </thead>
-          <tbody>
-            {cycles.map((cycle) => {
-              return (
-                <tr key={cycle.id}>
-                  <td>{cycle.task}</td>
-                  <td>{cycle.minutesAmount} minutos</td>
-                  <td>
-                    {formatDistanceToNow(cycle.startDate, {
-                      addSuffix: true,
-                      locale: ptBR,
-                    })}
-                  </td>
-                  <td>
-                    {cycle.finishedDate && (
-                      <Status color={"green"}>Concluído</Status>
-                    )}
 
-                    {cycle.interruptedDate && (
-                      <Status color={"red"}>Interrompido</Status>
-                    )}
-
-                    {!cycle.finishedDate && !cycle.interruptedDate && (
-                      <Status color={"yellow"}>Em andamento</Status>
-                    )}
-                  </td>
-                </tr>
-              );
-            })}
-          </tbody>
-        </table>
-      </HistoryList>
     </HistoryContainer>
   );
 };
